667ed4318ebb51adf4ad10e24e14b583973d1922
Ideophagous <antontheog@gmail.com>
2018-02-08 03:14:16 +0000

    separating image new size calculations

--- TifImageViewer.py
+++ TifImageViewer.py
@@ -3,13 +3,22 @@ import cv2
 import numpy as np
 import os
 
-class ImageViewer(wx.App):
-    def __init__(self,redirect=False, filename=None):
-        super(ImageViewer,self).__init__(redirect,filename)
+class ImageViewerFrame(wx.Frame):
+    def __init__(self,parent, title):
+        super(ImageViewerFrame,self).__init__(None,title=title)
         
-        self.frame = myFrame(None,title="Image Viewer")
-        
+        self.panel = myPanel(None,self)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.panel, 1, wx.EXPAND|wx.ALL, 2)
+        self.SetSizer(self.sizer)
+        self.CreateStatusBar()
+        self.SetStatusText("Made for Delmic")
         self.createMenuBar()
+        self.Show()
+
+        self.Bind(wx.EVT_SIZE, self.onResize)
+        
+        
 
     def createMenuBar(self):
         """
@@ -30,7 +39,7 @@ class ImageViewer(wx.App):
         menuBar.Append(fileMenu,"&File")
         menuBar.Append(helpMenu,"&Help")
 
-        self.frame.SetMenuBar(menuBar)
+        self.SetMenuBar(menuBar)
 
         self.Bind(wx.EVT_MENU,self.onExit,exitItem)
         self.Bind(wx.EVT_MENU,self.onAbout,aboutItem)
@@ -38,7 +47,7 @@ class ImageViewer(wx.App):
         
 
     def onExit(self,event):
-        self.frame.Close(True)
+        self.Close(True)
 
     def onAbout(self,event):
         
@@ -48,33 +57,17 @@ class ImageViewer(wx.App):
 
     def onOpenFile(self,event):
         wildcard = "All files (*.*) | *.*|(*.jpg)|*.jpg|(*.png)|*.png|(*.tif)|*.tif|(*.tiff)|*.tiff"
-        openFileDialog = wx.FileDialog(self.frame, "Open", "", "", 
+        openFileDialog = wx.FileDialog(self, "Open", "", "", 
                                        wildcard, 
                                        wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
-        ofd = openFileDialog.ShowModal()
-        if ofd == wx.ID_OK:
-            self.frame.panel.Destroy()
-            self.frame.panel = myPanel(openFileDialog.GetPath(),self.frame)
+        modal = openFileDialog.ShowModal()
+        if modal == wx.ID_OK:
+            self.panel.Destroy()
+            self.panel = myPanel(openFileDialog.GetPath(),self)
             
         
         openFileDialog.Destroy()
 
-    
-        
-class myFrame(wx.Frame):
-    def __init__(self,value,title):
-        super(myFrame,self).__init__(None,title=title)
-        self.panel = myPanel(None,self)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.panel, 1, wx.EXPAND|wx.ALL, 2)
-        self.SetSizer(sizer)
-        self.CreateStatusBar()
-        self.SetStatusText("Made for Delmic")
-        
-        self.Show()
-
-        self.Bind(wx.EVT_SIZE, self.onResize)
-    
     def onResize(self, event):
                 
         try:
@@ -83,73 +76,72 @@ class myFrame(wx.Frame):
             self.panel = myPanel(f,self)
         except Exception,e:
             print(str(e))
-        
-        
-        self.Refresh()
-        self.Layout()
+    
     
 
 class myPanel(wx.Panel):
     def __init__(self,filename,frame):
         super(myPanel,self).__init__(frame)
-        self.sizer = wx.GridBagSizer(5,5)
+        
         self.filename = filename
         frame_width,frame_height = frame.GetSize()
         self.SetSize((frame_width,frame_height))
-        if(self.filename!=None):
+        if(self.filename is not None):
             try:
                 extension = self.filename.split('.')[-1]
-                
-                if(extension in ['tiff','tif','TIF','TIFF']):
+                #we bypass the issue of TIF images by converting the image
+                #to a temporary jpg file
+                if(extension.lower() in ['tiff','tif']):
                     im = cv2.imread(self.filename).astype(np.uint8)
                     cv2.imwrite('temp.jpg', im)
                     self.filename = 'temp.jpg'
                 
                 self.image = wx.Image(self.filename,wx.BITMAP_TYPE_ANY)
                 
-                image_width,image_height = self.image.GetSize()
-                image_ratio = image_height/float(image_width)
-                if(image_ratio<1):
-                    image_frame_ratio = frame_width/float(image_width)
-                    
-                    if(image_height*image_frame_ratio>frame_height):
-                        image_height = frame_height
-                        
-                    else:
-                        image_height = image_height*image_frame_ratio
-                        
-                    image_width = int(image_height/image_ratio)
-                    self.image.Rescale(image_width,image_height)
-                    
-                else:
-                    image_frame_ratio = frame_height/float(image_height)
-                    
-                    if(image_width*image_frame_ratio>frame_width):
-                        image_width = frame_width
-
-                    else:
-                        image_width = image_width*image_frame_ratio
-                        
-                    image_height = int(image_width*image_ratio)
-                    self.image.Rescale(image_width,image_height)
+                image_width,image_height = get_new_image_size(self.image.GetSize(),frame.GetSize())
+                self.image.Rescale(image_width,image_height)
                 
                 self.image_control = wx.StaticBitmap(self, wx.ID_ANY, 
                                              wx.Bitmap(self.image))
-                self.sizer.Add(self.image_control,pos=(0,0),span=(5,5), flag=wx.EXPAND|wx.ALL, border=2)
-                self.SetSizer(self.sizer)
-                sizer = wx.BoxSizer(wx.VERTICAL)
-                sizer.Add(self, 1, wx.EXPAND|wx.ALL, 2)
-                frame.SetSizer(sizer)
                 
+            
             except Exception,e:
                 print(str(e))
         
+def get_new_image_size(image_size,frame_size):
+    image_width,image_height = image_size
+    frame_width,frame_height = frame_size
+    image_ratio = image_height/float(image_width)
+    if(image_ratio<1):
+        image_frame_ratio = frame_width/float(image_width)
+        
+        if(image_height*image_frame_ratio>frame_height):
+            image_height = frame_height
+            
+        else:
+            image_height = image_height*image_frame_ratio
+            
+        image_width = int(image_height/image_ratio)
+        
+        
+    else:
+        image_frame_ratio = frame_height/float(image_height)
+        
+        if(image_width*image_frame_ratio>frame_width):
+            image_width = frame_width
+
+        else:
+            image_width = image_width*image_frame_ratio
+            
+        image_height = int(image_width*image_ratio)
     
+    return image_width,image_height
         
 
 if __name__ == '__main__':
-    imageViewer = ImageViewer()
-    imageViewer.MainLoop()
+    app = wx.App()
+    imageViewerFrame = ImageViewerFrame(None, "Delmic Image Viewer")
+    app.MainLoop()
 
     #delete temporary image file if it exists
     try:
